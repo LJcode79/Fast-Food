ad2d03a485152f4490caae1ba215a363e45e97e8
LJ <ljdb1999@gmail.com>
2025-02-26 00:38:07 -0800

    Created models

--- FastFood.Models/Cart.cs
+++ FastFood.Models/Cart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace FastFood.Models
 {
     internal class Cart
     {
+        [Key]
         public int Id { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required,MinLength(1)]
+        public int Count { get; set; }
+
     }
 }
